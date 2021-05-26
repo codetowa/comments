61ef4321d9c4ac0b0f349d39d0389c97a334c002
Your Name <youremail@yourdomain.com>
2021-05-26 16:41:53 +0300

    deploy

---
+++ precache-manifest.8f0c697418aca81b13d86aca627fd3a6.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f2083d2cc192ca4325fc15548c4a83b",
+    "url": "/.well-known/assetlinks.json"
+  },
+  {
+    "revision": "6b728b4c838fbb0c8d2517c41998781b",
+    "url": "/404.html"
+  },
+  {
+    "revision": "44bd5300f7d196b77b654880cd31888c",
+    "url": "/assetlinks.json"
+  },
+  {
+    "revision": "ae92edd50091a48aded8",
+    "url": "/css/app.9433cbb8.css"
+  },
+  {
+    "revision": "00fdc1e6803afbf53c70",
+    "url": "/css/chunk-vendors.b1f8ff19.css"
+  },
+  {
+    "revision": "9a069f035ca2a9b1e3d4e7f99ed25909",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "3d1f8fa2f06249540889a7bbe69cf5bb",
+    "url": "/fonts/materialdesignicons-webfont.3d1f8fa2.eot"
+  },
+  {
+    "revision": "3e722fd57a6db80ee119f0e2c230ccff",
+    "url": "/fonts/materialdesignicons-webfont.3e722fd5.ttf"
+  },
+  {
+    "revision": "4187121a4353440c2a865dbf1bc1901b",
+    "url": "/fonts/materialdesignicons-webfont.4187121a.woff2"
+  },
+  {
+    "revision": "fec1b66adcf131415a2511bd77e747cc",
+    "url": "/fonts/materialdesignicons-webfont.fec1b66a.woff"
+  },
+  {
+    "revision": "853ad12e09240b243f917430f15ff280",
+    "url": "/icons/128.png"
+  },
+  {
+    "revision": "6721133c23bbb3702a3a30ab5379d761",
+    "url": "/icons/16.png"
+  },
+  {
+    "revision": "b978672de5a56a1e3dcb0edd61984a5c",
+    "url": "/icons/32.png"
+  },
+  {
+    "revision": "e8c2846016b1a9e82a0e5df5d127a49a",
+    "url": "/icons/48.png"
+  },
+  {
+    "revision": "26f9086eb792ec7c8c819134a883ee48",
+    "url": "/icons/64.png"
+  },
+  {
+    "revision": "0d2be44042f1da6a45986b3064afae2f",
+    "url": "/img/CoffeeCupCartoon.png"
+  },
+  {
+    "revision": "c5232f7af14cba41324ea17019467b56",
+    "url": "/img/Promo 1280x800.png"
+  },
+  {
+    "revision": "d96824c64094cab60684b5b4cf31fe99",
+    "url": "/img/Promo 440x280.png"
+  },
+  {
+    "revision": "40b9fb42e75582ab9b4e08b32949d2ed",
+    "url": "/img/google-btn.svg"
+  },
+  {
+    "revision": "7e8abaf8f00142d644622c1bec08dae3",
+    "url": "/img/icon.png"
+  },
+  {
+    "revision": "a183b2568145209bc21297a3bd056429",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "7e8abaf8f00142d644622c1bec08dae3",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "57d378414db25f8e54cd8e7a3b2376ce",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "a5a82e797433353801cee413b3066a81",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "e9a4f63a8ec8eb21f5263f5dcf046ff1",
+    "url": "/img/icons/apple-touch-icon-114x114.png"
+  },
+  {
+    "revision": "232217aa56b22a068d247baace786584",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "b7f4f7be91b8cc77e303f37e7c58c805",
+    "url": "/img/icons/apple-touch-icon-144x144.png"
+  },
+  {
+    "revision": "01c6301a4c64af933bc0ed726f5fead9",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "1db36417ef2460a9560f17432a6237b5",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "68d447e42ed78c3c81f13145c691b5ca",
+    "url": "/img/icons/apple-touch-icon-57x57.png"
+  },
+  {
+    "revision": "ef6ab5487876f016becd216a7b9d310f",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "9442bc1282d653349dc3a396e64b14e2",
+    "url": "/img/icons/apple-touch-icon-72x72.png"
+  },
+  {
+    "revision": "533451e62ea0fe943ad1e7079bd08152",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "533451e62ea0fe943ad1e7079bd08152",
+    "url": "/img/icons/apple-touch-icon-ipad-76x76.png"
+  },
+  {
+    "revision": "7e8abaf8f00142d644622c1bec08dae3",
+    "url": "/img/icons/apple-touch-icon-ipad-retina-152x152.png"
+  },
+  {
+    "revision": "7e8abaf8f00142d644622c1bec08dae3",
+    "url": "/img/icons/apple-touch-icon-iphone-60x60.png"
+  },
+  {
+    "revision": "232217aa56b22a068d247baace786584",
+    "url": "/img/icons/apple-touch-icon-iphone-retina-120x120.png"
+  },
+  {
+    "revision": "68d447e42ed78c3c81f13145c691b5ca",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "6721133c23bbb3702a3a30ab5379d761",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "b978672de5a56a1e3dcb0edd61984a5c",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "9a069f035ca2a9b1e3d4e7f99ed25909",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "0cffdd8483dc07f2510dcd4f6ed827c4",
+    "url": "/img/icons/icon.png"
+  },
+  {
+    "revision": "1f4a52700ee7250770d009770b59842f",
+    "url": "/img/icons/logo-outline-maskable.svg"
+  },
+  {
+    "revision": "809bfbb4926e0bc33e2c0c812fc6cbe9",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "252cf2c332aa0e28086ae672359818d4",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "640eb79f7d4693bfb8889fdcf86f5dc5",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "fa72e9d0272aff7bf6a683414e487572",
+    "url": "/img/logo - letter.svg"
+  },
+  {
+    "revision": "a9d76e2738112e6eaf4ac4be34c404c4",
+    "url": "/img/logo-outline.svg"
+  },
+  {
+    "revision": "640eb79f7d4693bfb8889fdcf86f5dc5",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "a934f1d213f0f7f462ac628c1a09af78",
+    "url": "/img/user-img-default-2.jpg"
+  },
+  {
+    "revision": "cd0ccf44370381d052f6945ee97274d2",
+    "url": "/img/user-img-default-faded.jpg"
+  },
+  {
+    "revision": "39eb0bbf2cc33ba02f53f8585004f820",
+    "url": "/img/user-img-default.jpg"
+  },
+  {
+    "revision": "256fd95664e7f4d629b0e68f2d9784df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ae92edd50091a48aded8",
+    "url": "/js/app.61603ce5.js"
+  },
+  {
+    "revision": "00fdc1e6803afbf53c70",
+    "url": "/js/chunk-vendors.4fb7d0ba.js"
+  },
+  {
+    "revision": "ce28b9ce7de400b6a446cc886f15f01b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "24ba47c438cdc2a5f239cbd67089e2b5",
+    "url": "/service-worker.js"
+  }
+]);
